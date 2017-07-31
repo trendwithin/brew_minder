a21d23209ec6f43dfcd037f9a9afb40b814bce6f
Michael Becco <e4e6d4d5@gmail.com>
2017-07-31 14:05:51 -0700

    Resolve Error w/ Webpack jQuery for Bootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs
+//= require jquery
+// require jquery-ujs
 //= require turbolinks
-//= jquery
-//= tether
+//= require tether
 //= require bootstrap-sprockets
 //= require_tree .
