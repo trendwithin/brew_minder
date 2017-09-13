0483780199188b2b70e89e6aff1eca2d4b56f530
Michael Becco <e4e6d4d5@gmail.com>
2017-09-13 15:14:46 -0700

    Local Copy of Chucks to scrape

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'pages/test'
   get 'pages/box'
+  get 'pages/chucks'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
