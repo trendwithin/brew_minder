b9da05e8f89e7785461e8383fc883cc323b33e3c
Michael Becco <e4e6d4d5@gmail.com>
2017-10-12 11:59:01 -0700

    Add Card Styling

--- app/javascript/components/display.js
+++ app/javascript/components/display.js
@@ -2,31 +2,28 @@ import React, { Component } from 'react';
 import BeerList from './beer_list';
 
 const Display = props => {
+
   const beerList = props.displayBeer.map((beer) => {
+    let uniqueKey = 0;
     return(
       <div className="row col-md-6 card card-block">
         <h1 className="card-title">{beer.name}</h1>
         <ul className="list-group card-text">
           {beer.beers.map((beer) => {
             return (
-              <  BeerList beer={beer} />
+              <BeerList beer={beer} />
             );
           })}
       </ul>
-        </div>
-
-
-
-
+    </div>
     );
   });
 
   return (
     <div className='container'>
-      <div className='row'>
-        <div id='search-results'>
+      <div id='search-results'>
+        <div className='row'>
           {beerList}
-
         </div>
       </div>
     </div>
