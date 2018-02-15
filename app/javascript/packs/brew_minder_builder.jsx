3b6090b26c99724d8a0b7ab2d5ef9db6090fa8d4
Michael Becco <e4e6d4d5@gmail.com>
2018-02-15 08:19:52 -0800

    Redesign Top Level Component for App

--- app/javascript/packs/brew_minder_builder.jsx
+++ app/javascript/packs/brew_minder_builder.jsx
@@ -1,59 +1,44 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import Grid from 'components/grid';
-import SearchBar from 'components/brewery_search_bar';
+import SearchBar from 'components/search_bar';
+import ConstantsList from '../utils/constant_files/wa_constants';
+import axios from 'axios';
+import { loadCheerioWith } from '../utils/loadCheerio';
+import { chunkArray, testData, fetchBrewer } from '../utils/helpers';
 
 class BrewMinderBuilder extends Component {
   state = {
     beerListings: [],
+    brewer: '',
   };
 
-  selectedBrewersName = (brewerName) => {
-    const brewers = [];
-    console.log(brewerName);
-  };
+  onFormSubmission = (term) => {
+    let brewers = [];
+    ConstantsList.BREW_RLS.map(function (establishment) {
+      this.setState({ brewer: term });
+      let establishmentURL = Object.values(establishment);
+      axios.get(establishmentURL).then(res => {
+        let k = _.keys(establishment);
+        let vals = loadCheerioWith(res, establishment, term);
+        let key = k[0];
+        let obj = {};
+        obj.establishment = key;
+        obj.beers = vals;
+        brewers.push(obj);
+        this.setState({ beerListings: brewers });
+      });
+    }, this);
 
-  chunkArray = (arr, sizeOfChunk) => {
-    const chunkedArray = [];
-    for (let i = 0; i < arr.length; i += sizeOfChunk) {
-      chunkedArray.push(arr.slice(i, i + sizeOfChunk));
-    }
-
-    return chunkedArray;
+    const formatData = chunkArray(brewers, 2);
   };
 
   render() {
-    const dummyData = [
-      {
-        establishment: 'chucks',
-        beers: ['one', 'two', 'three'],
-      },
-      {
-        establishment: 'pinebox',
-        beers: ['four', 'five', 'six'],
-      },
-      {
-        establishment: 'fake',
-        beers: ['seven', 'eight', 'nine'],
-      },
-      {
-        establishment: 'data',
-        beers: ['ten', 'eleven', 'twelve'],
-      },
-      {
-        establishment: 'five',
-        beers: ['ten', 'eleven', 'twelve'],
-      },
-    ];
-
-    const formattedData = this.chunkArray(dummyData, 2);
     return (
       <div>
-        <SearchBar
-          onSearchBarSubmit={this.selectedBrewersName}
-        />
+        <SearchBar onFormSubmission={this.onFormSubmission}/>
         <Grid
-          data={formattedData}
+          data={this.state.beerListings}
         />
       </div>
     );
